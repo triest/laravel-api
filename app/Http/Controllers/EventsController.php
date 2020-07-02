b79d42936ccbbc5e2809dc70b4b7019e681099d1
triest <you@example.com>
2020-07-02 20:50:42 +0300

    Add delete User from event. Not final

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -38,8 +38,29 @@
                 return response('user not found', 404);
             }
 
-                        $event->user()->attach($user);
-            
+            $event->user()->attach($user);
+
+            return response()->json($event->user()->get());
+        }
+
+        public function deleteUser($id, $userid = null, Request $request)
+        {
+            $event = Event::get($id);
+            if ($event == null) {
+                return response('event not found', 404);
+            }
+            if ($userid == null) {
+                return response('user not found', 404);
+            }
+
+            $user = User::get($userid);
+            if ($user == null) {
+                return response('user not found', 404);
+            }
+
+
+            $event->user()->detach($user);
+
             return response()->json($event->user()->get());
         }
     }
