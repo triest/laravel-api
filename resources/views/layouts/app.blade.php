9f4c21cce9a7dac06c60cf4fbb94b0875a4c58f0
triest <you@example.com>
2020-07-05 11:41:34 +0300

    Fix name and readme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{Auth::user()->id}}  {{Auth::user()->last_name}} 2<span class="caret"></span>
+                                    {{Auth::user()->first_name}}  {{Auth::user()->last_name}} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
